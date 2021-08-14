a68204028a88a0a094936374a0549021b44f4d92
CameronH926 <cholbrook@cub.uca.edu>
2021-08-13 22:23:03 -0500

    Got a super basic weather API call going, as well as hid my API key from the source code. Also using pretty print with this project.

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import requests
+from dotenv import load_dotenv
+import os
+import pprint
 
-print("Hey, listen!")
+load_dotenv()
+
+APIKey = os.getenv("API_KEY")
+
+cityName = input("Enter a city: ")
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}"
+
+result = requests.get(url)
+
+data = result.json()
+
+pprint.pprint(data)
+
