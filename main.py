5670bcfa23cb451c7f43d1d845c9a7244eb83d30
CameronH926 <cholbrook@cub.uca.edu>
2021-08-18 17:09:12 -0500

    Learning more of the OpenWeatherMap API. For testing purposes, I made an option to choose between city name and city id. Also, I removed my SQL root password from the code.

--- main.py
+++ main.py
@@ -2,18 +2,77 @@ import requests
 from dotenv import load_dotenv
 import os
 import pprint
+import mysql.connector
 
+########## SETUP ##########
+
+# dotenv, for environment variables and protection of API key.
 load_dotenv()
 
-APIKey = os.getenv("API_KEY")
+########## END SETUP ##########
 
-cityName = input("Enter a city: ")
+def connectToDatabase():
 
-url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIKey}"
+    # MySQL connector setup
+    db = mysql.connector.connect(
 
-result = requests.get(url)
+        host="localhost", user="root",
+        password=os.getenv("ROOT"),
+        database="test_weather_database"
 
-data = result.json()
+    )
 
-pprint.pprint(data)
+    myCursor = db.cursor()
 
+    myCursor.execute("show databases;")
+
+    myCursor.execute("use database test_weather_database")
+
+
+def printWeatherData(data):
+
+    # Print that weather data, for testing
+    pprint.pprint(data)
+
+
+def main():
+
+    # connectToDatabase()
+
+    """Main, where the program starts."""
+
+    # Main program start
+
+    selection = input("Which would you like to do?\n\n1. City\n2. City ID\n\n")
+
+    if selection == 1:
+
+        cityName = input("Enter a city name: ")
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={os.getenv('API_KEY')}&units=imperial"
+        result = requests.get(url)
+
+        # Store that URL into a printable variable
+        data = result.json()
+
+        printWeatherData(data)
+
+
+    else:
+        cityID = input("Enter a city ID: ")
+
+        url2 = f"http://api.openweathermap.org/data/2.5/weather?id={cityID}&appid={os.getenv('API_KEY')}&units=imperial"
+        result = requests.get(url2)
+
+        # Store that URL into a printable variable
+        data = result.json()
+
+        # Original: 4626286
+        # Secondary? 4113067
+
+        printWeatherData(data)
+
+
+
+if __name__ == '__main__':
+    main()
