9e1a4d10d6b7dc016c62413570839352d470fffb
CameronH64 <77362445+CameronH64@users.noreply.github.com>
2021-09-02 14:11:59 -0500

    Adding a laptop quickstart .bat file.

---
+++ laptop-quickstart-daily-weather-retrieval-tool.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start C:\"64-bit Programs"\"PyCharm Community Edition 2020.2"\bin\pycharm64.exe
+
+start chrome https://github.com/CameronH64/daily-weather-retrieval-tool
+
+start C:\"64-Bit Programs"\Git\git-bash.exe
+
+cd C:\"Program Files"\MySQL\"MySQL Server 8.0"\bin
+mysql -u root -p
