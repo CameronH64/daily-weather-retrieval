a3bc028e2d516aa4690a0e66819fde0cce00b12d
CameronH926 <cholbrook@cub.uca.edu>
2021-10-02 23:10:47 -0500

    Added a .py header on the top to give a description and outline for the code. Also oulined the needed planned Tkinter widgets.

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
+"""
+Cameron Holbrook
+
+Project Description:
+
+This program will grab current weather information
+and store it in a local MySQL database.
+
+
+Outline:
+
+    Import statements
+    Load dotenv for API key
+    Setup the Tkinter window
+    Python functions
+    Create GUI elements (Label, Entry, etc.)
+    Run the Tkinter mainloop.
+    Reference materials
+
+
+"""
+
 import requests             # Used to call the HTTP API.
 from dotenv import load_dotenv      # Used to store the API key
 import os                   # Used to access environmental variables, and my API key.
 import pprint               # Simply prints out .json output in a much neater format.
 import mysql.connector      # Used to run SQL commands in Python
 from tkinter import *
-from PIL import ImageTk, Image
-import datetime
+# from PIL import ImageTk, Image
+# import datetime
 
 # dotenv, for environment variables and protection of API key.
 load_dotenv()
+
+# Setup the Tkinter window.
+root = Tk()
+root.geometry("480x300")
+
+# Plain OpenWeatherMap API calls
+# https:/api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+OWMCity = "http:/api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+# https:/api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
+OWMCityID = "http:/api.openweathermap.org/data/2.5/weather?id={}&appid={}"
+# https:/api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+OWMCoordinates = "http:/api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
+# https:/api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+OWMZipCode = "http:/api.openweathermap.org/data/2.5/weather?zip={},{}&appid={}"
 
 # Ignoring this for now; getting OWM stuff down first.
 def connectToDatabase():
@@ -26,16 +62,7 @@ def connectToDatabase():
 
     )
 
-# Plain OpenWeatherMap API calls
-# https:/api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-OWMCity = "http:/api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-# https:/api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
-OWMCityID = "http:/api.openweathermap.org/data/2.5/weather?id={}&appid={}"
-# https:/api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-OWMCoordinates = "http:/api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
-# https:/api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
-OWMZipCode = "http:/api.openweathermap.org/data/2.5/weather?zip={},{}&appid={}"
-
+# Will call the OWM API, and return the API call weather data
 def OWMCall(choice):
 
     if choice == "City":
@@ -60,41 +87,44 @@ def OWMCall(choice):
 # Greenbrier, TN:        4626286
 # Greenbrier, AR:        4113067
 
-root = Tk()
-root.geometry("800x400")
-
 def searchButtonClicked():
     return searchEntry.get()
 
-######################### CREATE ELEMENTS FOR GUI #########################
 
-# LABEL for search parameters
-question = Label(root, text="Enter your search parameters: ")
-question.grid(row=0, column=0)
+# ================= CREATE ELEMENTS FOR GUI =================
 
-# ENTRY textbox for search parameters
+# ENTRY box, search box
 searchEntry = Entry(root, width=40)
 searchEntry.grid(row=1, column=0)
 
-# BUTTON for submit
-submissionButton = Button(root, text="Search", command=lambda: searchButtonClicked())
-submissionButton.grid(row=1, column=1)
 
 # LABEL widget for API call return
 output = Label(root, wraplength=500)
 output.grid(row=2, column=0)
 
-#======================== END CREATE ELEMENTS FOR GUI ============
 
-# returnedData = OWMCall("City")
-# pprint.pprint(returnedData)
+# BUTTON, submission button
+submissionButton = Button(root, text="Search", command=lambda: searchButtonClicked())
+submissionButton.grid(row=1, column=1)
 
-# Activate the mainloop window
+
+# RADIO buttons, search choices
+
+
+# LABEL, units choice
+
+
+# RADIO buttons, temperature choice
+
+
+# BUTTON, save to database
+
+
+# Activate Tkinter's mainloop window
 root.mainloop()
 
-APICall = requests.get("http:/api.openweathermap.org/data/2.5/weather?q=conway&appid=3f50437fe51d585ab8a11be93c75efc3")
-data = APICall.json()
-pprint.pprint(data)
+
+# ================= REFERENCE MATERIALS =================
 
 # Basically, all Tkinter is:
 
